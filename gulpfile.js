11c81555d755a7e0655fc6b5e642972dcd0dcd70
Arnaud Dufranne <dufrannea@gmail.com>
2015-10-03 17:27:27 +0200

    better gulpFile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,24 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var typescript = require('gulp-typescript')
 
 gulp.task("clean", function(){
-	gulp
+	return gulp
 		.src(['build/**/*'])
-		.pipe(gulp.remove());
+		.pipe(clean());
 });
+
+gulp.task('build-static', function(){
+	return gulp.src([
+		'src/**/*.html',
+		'src/**/*.css'])
+		.pipe(gulp.dest('build'));
+})
+
+gulp.task('build', ["clean", "build-static"], function(){
+	var tsProject = typescript.createProject('tsconfig.json');
+	var tsResult = tsProject.src()
+			.pipe(typescript(tsProject));
+
+    return tsResult.js.pipe(gulp.dest('build'));
+})
