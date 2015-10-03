617654ed36ab8f24735c0d55f6a0dfad9d3e2997
Arnaud Dufranne <dufrannea@gmail.com>
2015-10-03 19:13:29 +0200

    adapting bower for build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
-var typescript = require('gulp-typescript')
+var typescript = require('gulp-typescript');
+var install = require('gulp-install');
+var mainBowerFiles = require('main-bower-files');
 
 gulp.task("clean", function(){
 	return gulp
-		.src(['build/**/*'])
+		.src(['build'])
 		.pipe(clean());
 });
 
@@ -15,10 +17,43 @@ gulp.task('build-static', function(){
 		.pipe(gulp.dest('build'));
 });
 
-gulp.task('build', ["clean", "build-static"], function(){
+/**
+ * build for dev
+ */
+gulp.task('build-typescript', function(){
 	var tsProject = typescript.createProject('tsconfig.json');
 	var tsResult = tsProject.src()
 			.pipe(typescript(tsProject));
 
     return tsResult.js.pipe(gulp.dest('build'));
 });
+
+/**
+ * Install all packages needed by the solution.
+ */
+gulp.task("bower-install",function(){
+	return gulp
+		.src(['./bower.json'])
+  		.pipe(install());
+});
+
+/**
+ * Copy needed packages to build.
+ */
+gulp.task("build-bower", function(){
+	var mainFiles = mainBowerFiles();
+	return gulp
+			.src(mainFiles)
+			.pipe(gulp.dest('build/main/bower_components'))
+})
+
+/**
+ * build for dev
+ */
+gulp.task('build', ["build-typescript","build-static"]);
+
+/**
+ * Rebuild : clean, build static and typescript files,
+ * and install bower deps.
+ */
+gulp.task('rebuild', ["build-bower", "build"]);
