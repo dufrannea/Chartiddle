32e75c56e7d2a7024a1a37b2bb7f19dc5763bce1
Arnaud Dufranne <dufrannea@gmail.com>
2015-10-04 19:51:15 +0200

    adding release

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
+// Node modules
+var fs = require('fs'), 
+	vm = require('vm'), 
+	merge = require('deeply'); 
+
+// Gulp and plugins
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 var typescript = require('gulp-typescript');
 var install = require('gulp-install');
 var mainBowerFiles = require('main-bower-files');
-var sass = require('gulp-sass');
-
+var sass = require('gulp-sass'),
+	rjs = require('gulp-requirejs-bundler'),
+    uglify = require('gulp-uglify'),	
+    htmlreplace = require('gulp-html-replace');
+    
+/**
+ * TASKS DEFINITION
+ */
+    
 gulp.task("clean", function(){
 	return gulp
 		.src(['build'])
@@ -55,6 +68,43 @@ gulp.task("build-bower", function(){
 	return gulp
 			.src(mainFiles)
 			.pipe(gulp.dest('build/main/bower_components'))
+})
+
+gulp.task('release-css', ['rebuild'], function(){
+    return gulp.src('build/main/styles/style.css')
+               .pipe(gulp.dest("dist"))
+})
+
+gulp.task('release-html', ['rebuild'], function(){
+    return gulp.src('build/main/index.html')
+        .pipe(htmlreplace({
+            'js': 'scripts.js',
+            'css': 'style.css'
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('release', ['release-html', 'release-css'], function(){
+	
+    // read runtime Config
+	var requireJsRuntimeConfig = vm.runInNewContext(fs.readFileSync('build/main/require.config.js') + '; require;');
+    requireJsOptimizerConfig = merge(requireJsRuntimeConfig, {
+        out: 'scripts.js',
+        baseUrl: './build/main',
+        name: 'app/main',
+        paths: {
+            requireLib: 'bower_components/require'
+        },
+        insertRequire : ["app/main"],
+        include: [
+            'requireLib'
+        ],
+        bundles: {
+        }
+    });
+    return rjs(requireJsOptimizerConfig)
+        .pipe(uglify({ preserveComments: 'none' }))
+        .pipe(gulp.dest('./dist'));
 })
 
 /**
