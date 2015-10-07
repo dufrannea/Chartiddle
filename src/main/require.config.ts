bd8f687a6ed9abbf9bccb51b2698075a8b1bdc1c
Arnaud Dufranne <dufrannea@gmail.com>
2015-10-07 20:26:23 +0200

    Adds new dependencies and typings : - papaparse - dropzone

--- src/main/require.config.ts
+++ src/main/require.config.ts
@@ -8,7 +8,9 @@ require = <any>{
 		react : 'bower_components/react',
 		flux : 'bower_components/Flux',
 		bootstrap : "bower_components/bootstrap.min",
-		jquery : "bower_components/jquery.min"
+		jquery : "bower_components/jquery.min",
+		dropzone: 'bower_components/dropzone/dist/min/dropzone.min.js',
+		papaparse: 'bower_components/papaparse/papaparse.js'
 	},
 	shim : {
 		bootstrap : ['jquery']
